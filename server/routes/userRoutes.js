983b9b0ab24d1a1f98ec55b932e638ebe81384ed
Nipuna Kaweya <nipuna609@gmail.com>
2025-08-04 23:09:24 +0530

    Routes updated

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import {auth} from "../middleware/auth.js";
+import {isAdmin} from "../middleware/isAdmin.js";
+import {createAdmin, getAllUsers, getUserById, updateUserInfo} from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.post('/create-admin',auth,isAdmin,createAdmin);
+router.get('/',auth,isAdmin,getAllUsers);
+router.put('/update',auth,updateUserInfo);
+router.put('/:id',auth,getUserById);
+
+export default router;
