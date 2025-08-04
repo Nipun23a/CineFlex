72d0a79dd4bf5d168244d598baf9087965c9e5ea
Nipuna Kaweya <nipuna609@gmail.com>
2025-08-04 22:37:09 +0530

    Models and New packages added

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const bookingSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     showtime: { type: mongoose.Schema.Types.ObjectId, ref: 'Showtime', required: true },
+    theater: { type: mongoose.Schema.Types.ObjectId, ref: 'Theater', required: true },
     seats: [{ type: Number, required: true }],
     totalPrice: { type: Number, required: true },
     paymentStatus: { type: String, enum: ['pending', 'paid', 'failed'], default: 'pending' },
