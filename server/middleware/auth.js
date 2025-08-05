491eb90066e0c43bee7eb999b1c50d4b91eae2db
Nipuna Kaweya <nipuna609@gmail.com>
2025-08-05 23:06:45 +0530

    Auth Middleware Updated

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,11 +3,11 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = process.env.JWT_SECRET ;
 
 export const auth = (req,res,next) => {
-    const token  = req.headers.authorization?.split('')[1];
+    const token  = req.headers.authorization?.split(' ')[1];
     if (!token) return res.status(401).json({message: 'Access Denied'});
 
     try {
-        req.user = jwt.verify(token, JWT_SECRET);
+        req.user = jwt.verify(token, process.env.JWT_SECRET);
         next();
     }catch (error){
         res.status(401).json({message:"Invalid token"});
