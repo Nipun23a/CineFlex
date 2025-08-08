c7ca7bfb4acccb63445e7115f34baa1d3ed94e98
Nipuna Kaweya <nipuna609@gmail.com>
2025-08-08 12:45:55 +0530

    All The Backed API's Added for Frontend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -31,6 +31,26 @@ export const updatePassword = async (passwordData) => {
     return await axiosInstance.put('/auth/update-password', passwordData);
 };
 
+// ===================== USER APIs =====================
+// Create Admin (Admin Only)
+export const createAdmin = async (adminData) => {
+    return await axiosInstance.post('/user/create-admin', adminData);
+}
+
+// Get All User (Admin Only)
+export const getAllUsers = async () => {
+    return await axiosInstance.get('/user');
+}
+
+// Update User Information
+export const updateUser = async (userData) => {
+    return await axiosInstance.post('/user/update',userData);
+}
+
+// Get User ID
+export const getUserById = async (id) => {
+    return await axiosInstance.get(`/user/${id}`);
+}
 
 // ===================== MOVIE APIs =====================
 
@@ -58,4 +78,78 @@ export const updateMovie = async (id, movieData) => {
 export const deleteMovie = async (id) => {
     return await axiosInstance.delete(`/movies/${id}`);
 };
+
+
+// ===================== THEATER HALL APIs =====================
+// Create a Theater (Admin Only)
+export const createTheater = async (theaterData) => {
+    return await axiosInstance.post('/theaters', theaterData);
+}
+
+// Get All
+export const getAllTheater = async () =>{
+    return await axiosInstance.get('/theaters')
+}
+
+// Get Theater By ID
+export const getTheaterById = async (id) =>{
+    return await axiosInstance.get(`/theaters/${id}`);
+}
+
+// Update Theater (Admin Only)
+export const updateTheater = async (id, theaterData) => {
+    return await axiosInstance.put(`/theaters/${id}`,theaterData);
+}
+
+// Delete Theater (Admin Only)
+export const deleteTheater = async (id) =>{
+    return await axiosInstance.delete(`/theaters/${id}`);
+}
+
+// ===================== SHOWTIME APIs =====================
+
+// Create a showtime (Admin Only)
+export const createShowTime = async (showTimeData) => {
+    return await axiosInstance.post('/showtimes',showTimeData);
+}
+
+// Get All Showtime
+export const getAllShowTimes = async () => {
+    return await axiosInstance.get('/showtimes');
+}
+
+// Get Showtime by ID
+export const getShowTimeById = async (id) =>{
+    return await axiosInstance.get(`/showtimes/${id}`);
+}
+
+// Update the Showtime (Admin Only)
+export const updateShowTime = async (id,showTimeData) => {
+    return await axiosInstance.put(`/showtimes/${id}`,showTimeData)
+}
+
+// Delete the Showtime (Admin Only)
+export const deleteShowTime = async (id) => {
+    return await axiosInstance.delete(`/showtimes/${id}`);
+}
+
+// ===================== BOOKING APIs =====================
+// Create Booking
+export const createBooking = async (bookingData) => {
+    return await axiosInstance.post('/bookings',bookingData);
+}
+// Get All Booking
+export const getAllBooking = async () =>{
+    return await axiosInstance.get('/bookings');
+}
+// Get Booking By Id
+export const getBookingById = async (id) => {
+    return await axiosInstance.get(`/bookings/${id}`)
+}
+
+// Get Booking By User Id
+export const getBookingByUser = async (userId) => {
+    return await axiosInstance.get(`/bookings/user/${userId}`)
+}
+
 
